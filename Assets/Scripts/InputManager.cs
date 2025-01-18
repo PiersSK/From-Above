626ebec47493d4964f412e2970a60757517cf56d
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2025-01-18 22:59:43 +0000

    adds player move and look

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class InputManager : MonoBehaviour 
+{
+    private PlayerInput playerInput;
+    private PlayerInput.PlayerActions playerActions;
+    private PlayerMotor motor;
+    private PlayerLook look;
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        playerActions = playerInput.Player;
+
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
+        playerActions.Jump.performed += ctx => motor.Jump();
+        playerActions.Crouch.performed += ctx => motor.Crouch();
+        playerActions.Sprint.performed += ctx => motor.Sprint();
+    }
+
+    private void Update()
+    {
+        motor.ProcessMove(playerActions.Move.ReadValue<Vector2>());
+
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(playerActions.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        playerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerActions.Disable();
+    }
+}
