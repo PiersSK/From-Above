09ac22d94b349a08d9b2d30c47d441ea892f1c33
Piers Stuart-Kelso <piers.stuart-kelso@hotmail.co.uk>
2025-01-21 13:10:25 +0000

    adds button disabler

--- Assets/Scripts/Interactables/CyclePowerButton.cs
+++ Assets/Scripts/Interactables/CyclePowerButton.cs
@@ -5,6 +5,7 @@ public class CyclePowerButton : Interactable
     [SerializeField] private Animation lightFlicker;
     [SerializeField] private PlayerLook playerLook;
     private Animator buttonAnimator;
+    private bool powerNeedsCycling = true;
 
     private void Start()
     {
@@ -13,8 +14,15 @@ public class CyclePowerButton : Interactable
 
     protected override void Interact(Transform player)
     {
-        buttonAnimator.SetTrigger("Press");
-        lightFlicker.Play();
-        playerLook.CameraShake(5f, 5f, true);
+        if (powerNeedsCycling)
+        {
+            buttonAnimator.SetTrigger("Press");
+            lightFlicker.Play();
+            playerLook.CameraShake(5f, 5f, true);
+            powerNeedsCycling = false;
+        } else
+        {
+            buttonAnimator.SetTrigger("Press");
+        }
     }
 }
