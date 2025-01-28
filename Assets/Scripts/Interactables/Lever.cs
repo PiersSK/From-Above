c45c7d17260d7c22addeb45bdb19384aa5c4ba62
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2025-01-28 01:43:34 +0000

    adds weapons

--- Assets/Scripts/Interactables/Lever.cs
+++ Assets/Scripts/Interactables/Lever.cs
@@ -6,6 +6,8 @@ public class Lever : Interactable
     [SerializeField] private Task task;
     private bool leverIsUp = false;
 
+    [SerializeField] private Animator weaponAnim;
+
     public override bool CanInteract()
     {
         return !leverIsUp && TaskManager.Instance.tasks.Contains(task);
@@ -20,6 +22,7 @@ public class Lever : Interactable
         if(DoomsdayStatusUI.Instance.weaponLeversPulled == 3)
         {
             TaskManager.Instance.CompleteTask(task);
+            weaponAnim.SetTrigger("Deploy");
         }
     }
 }
--- Assets/Scripts/Interactables/ServerEXEs/SwordInitiation.cs
+++ Assets/Scripts/Interactables/ServerEXEs/SwordInitiation.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class SwordInitiation : ServerEXE
 {
     private bool initiationDone = false;
-
     public override void RunExe()
     {
         TaskManager.Instance.CompleteTask(task);
