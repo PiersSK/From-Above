870edb63a48ffcfc09ac7650545d3c526b64c7ee
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2025-01-29 15:53:23 +0000

    adds warmpup sfx

--- Assets/Scripts/Interactables/ServerButton.cs
+++ Assets/Scripts/Interactables/ServerButton.cs
@@ -4,6 +4,7 @@ public class ServerButton : Interactable
 {
     public ServerEXE exe;
     public bool usableInPhaseOne = false;
+    [SerializeField] private AudioClip sfx;
 
     public override bool CanInteract()
     {
@@ -13,6 +14,7 @@ public class ServerButton : Interactable
 
     protected override void Interact(Transform player)
     {
+        SoundManager.Instance.PlaySFXOneShot(sfx, 0, 0.3f);
         exe.RunExe();
     }
 }
