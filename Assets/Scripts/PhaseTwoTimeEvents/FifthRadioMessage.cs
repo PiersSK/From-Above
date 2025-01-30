1b845c3ba42fd95848bafcb01f58373b54286a06
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2025-01-30 23:51:55 +0000

    Reverts timer back to 10 mins, readjusts times for radio messages.

--- Assets/Scripts/PhaseTwoTimeEvents/FifthRadioMessage.cs
+++ Assets/Scripts/PhaseTwoTimeEvents/FifthRadioMessage.cs
@@ -6,6 +6,6 @@ public class FifthRadioMessage : TimedEvent
     public override bool ShouldEventTrigger()
     {
         return !hasBeenTriggered  && TimeController.Instance.RadioMessageTimeHasPassed(eventMinute, eventSecond) && TimeController.Instance.radioMessagesPlayed == 4 && TaskManager.Instance.phaseTwoTasksCompleted >= 4
-        || !hasBeenTriggered && TimeController.Instance.getOnWithItTimer >= 90f && TimeController.Instance.getOnWithItMessagePlayed;
+        || !hasBeenTriggered && TimeController.Instance.getOnWithItTimer >= 180f && TimeController.Instance.getOnWithItMessagePlayed;
     }
 }
