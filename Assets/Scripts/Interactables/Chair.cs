88d0da70c0ff8de898f76af0ef3b57cf1318f485
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2025-01-30 23:36:04 +0000

    stops chairs working at start too

--- Assets/Scripts/Interactables/Chair.cs
+++ Assets/Scripts/Interactables/Chair.cs
@@ -4,7 +4,12 @@ public class Chair : Interactable
 {
     [SerializeField] private Transform sitPoint;
     [SerializeField] private AudioClip sfx;
+    [SerializeField] private PlayerMotor motor;
 
+    public override bool CanInteract()
+    {
+        return !motor.movementOverridden;
+    }
     protected override void Interact(Transform player)
     {
         player.GetComponent<PlayerMotor>().ForcePlayerToPoint(sitPoint, true);
