42b2b5f8804c1cf3d6488cb49cd864ea79ae1f5e
OrcaOfTime <mwilliams.sean@gmail.com>
2025-04-16 21:17:54 +0100

    feat(code): Added Task list and some temp note comments

--- Assets/Scripts/Phases/Phase.cs
+++ Assets/Scripts/Phases/Phase.cs
@@ -7,8 +7,17 @@ public class Phase : ScriptableObject
 {
     [SerializeField] public AudioClip taskBeep;
     [SerializeField] public GameObject taskPad;
-    [SerializeField] public TextMeshProUGUI taskPadTrigger;
+    [SerializeField] public GameObject taskPadTrigger;
     [SerializeField] public TextMeshProUGUI taskCount;
     [SerializeField] public TextMeshProUGUI timer;
+    [SerializeField] public List<Task> tasks;
     [SerializeField] public List<GameObject> taskBlocks;
 }
+
+//!!!Notes I am to delete later!!!
+//Fun things we've learnt today:
+//1. You CANNOT reference an instance of an object from the game scene into a scriptable object
+//2. We current use almost EXCLUSIVELY instances of in scene objects in the task manager for phase tracking
+// This is not a criticism of the current system, it just means I can't transition my shiney new SO into 
+// the task manager as incrementally as I had hoped.
+//Time to crack open the crowbars 
