40b8dcc35b2abc0963f157b9ab19d479e9f310ec
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2025-04-20 18:33:45 +0100

    feat(code): adds doors, utility and tidies scene

--- Assets/Scripts/TaskStateChange.cs
+++ Assets/Scripts/TaskStateChange.cs
@@ -6,19 +6,43 @@ public class TaskStateChange : MonoBehaviour
     {
         Disappear,
         Appear,
-        LightEnable
+        LightEnable,
+        InvokeInteractable
     }
     [SerializeField] private ChangeType type;
 
     [SerializeField] private Task task;
 
+    [SerializeField] private Interactable interactable;
+    [SerializeField] private string interactableFunctionToInvoke;
+
+    private bool triggered = false;
+
+    private void Start()
+    {
+        if (type == ChangeType.InvokeInteractable && (interactable == null || interactableFunctionToInvoke == string.Empty))
+            Debug.LogWarning("Object " + gameObject.name + "has TaskStateChange type of InvokeInteractable but no interactable was provided as a reference");
+    }
+
     private void Update()
     {
-        if(TaskManager.Instance.tasks.Contains(task))
+        if(TaskManager.Instance.tasks.Contains(task) && !triggered)
         {
             if (type == ChangeType.Disappear) Destroy(gameObject);
             else if (type == ChangeType.Appear) GetComponent<Renderer>().enabled = true;
             else if (type == ChangeType.LightEnable) GetComponent<Light>().enabled = true;
+            else if (type == ChangeType.InvokeInteractable)
+            {
+                if (interactable == null || interactableFunctionToInvoke == string.Empty)
+                {
+                    Debug.LogWarning("Object " + gameObject.name + "has TaskStateChange type of InvokeInteractable but no interactable was provided as a reference. Nothing happened when triggered");
+                } else
+                {
+                    interactable.Invoke(interactableFunctionToInvoke, 0f);
+                }
+            }
+
+            triggered = true;
         }
     }
 }
