103b3edd451270d439ece9844a5b8c4dfed5d36a
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2025-04-21 13:55:44 +0100

    feat(code): Add VLM GameObject, add PhaseTransition & TaskCompletion logic

--- Assets/Scripts/Audio/VoiceLineManager.cs
+++ Assets/Scripts/Audio/VoiceLineManager.cs
@@ -1,16 +1,87 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 
 public class VoiceLineManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<VoiceLine> allVoiceLines;
+    private AudioSource audioSource;
+    private List<VoiceLine> playedVoiceLines = new List<VoiceLine>();
+
+    private float timeOfLastVoiceLine;
+    
+    private void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+
+        TaskManager.TaskCompleted += TaskCompleted;
+        TaskManager.PhaseChanged += PhaseChanged;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        EvaluateVoiceLines();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        TaskManager.TaskCompleted -= TaskCompleted;
+        TaskManager.PhaseChanged -= PhaseChanged;
+    }
+
+    private void TaskCompleted(Task task)
+    {
+        EvaluateVoiceLines();
+    }
+
+    private void PhaseChanged()
+    {
+        EvaluateVoiceLines();
+    }
+
+    private void EvaluateVoiceLines()
+    {
+        foreach (var vl in allVoiceLines)
+        {
+            if (playedVoiceLines.Contains(vl))
+                continue;
+
+            if (!AreTaskConditionsSatisfied(vl))
+                continue;
+
+            if (vl.playOnPhaseTransition && TaskManager.Instance.isPhaseTwo)
+            {
+                PlayVoiceLine(vl);
+            }
+            else if (vl.playOnTaskCompletion && AreTaskConditionsSatisfied(vl))
+            {
+                PlayVoiceLine(vl);
+            }
+        }
+    }
+
+    private bool AreTaskConditionsSatisfied(VoiceLine vl)
+    {
+        if (vl.taskTrigger != null)
+        {
+            if (!TaskManager.Instance.completedTasks.Contains(vl.taskTrigger))
+                return false;
+        }
+        return true;
+    }
+    private void PlayVoiceLine(VoiceLine vl)
+    {
+        if (vl.clip != null)
+        {
+            audioSource.PlayOneShot(vl.clip);
+            playedVoiceLines.Add(vl);
+            timeOfLastVoiceLine = TimeController.Instance.time;
+            Debug.Log($"[VoiceLineManager] Played {vl.name} at {timeOfLastVoiceLine}s");
+        }
     }
 }
