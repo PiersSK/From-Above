c2b823ea33eb8ed4feae73830306cd77393e38ff
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2025-04-22 10:06:33 +0100

    feat(code): add Inactivity TimeConditionType, neaten VoiceLine SO

--- Assets/Scripts/Audio/VoiceLines/VoiceLine.cs
+++ Assets/Scripts/Audio/VoiceLines/VoiceLine.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 public class VoiceLine : ScriptableObject
 {
     public AudioClip clip;
+    [Header("Trigger Conditions")]
     public bool playOnTimeCondition;
     public bool playOnPhaseTransition;
     public bool playOnTaskCompletion;
-
     public enum TimeConditionType
     {
         AbsoluteFromStart,
         AbsoluteFromEnd,
         RelativeToLastVoiceLine,
+        PeriodOfInactivity
     }
+    [Header("Time Trigger Conditions")]
     public TimeConditionType timeConditionType;
     public int absoluteSecondsIntoPhase;
     public int absoluteSecondsBeforePhaseEnd;
     public int percentageThroughPhase;
     public int secondsSinceLastVoiceLine;
-
+    public int secondsOfInactivity;
+    public Task cutOffTask;
+    
+    [Header("Task Trigger Conditions")]
     public Task taskTrigger;
 }
