6e14f0da3ab7f9fc32adfe6cd223816437bacece
OrcaOfTime <mwilliams.sean@gmail.com>
2025-05-26 17:24:14 +0100

    feat(code): Added Deploy Wheel Interact Script
    Initial script for deployment wheel created.
    Basically a copy and paste job from the levers.

---
+++ Assets/Scripts/Interactables/DeployWheel.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class DeployWheel : Interactable
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private Task task;
+    [SerializeField] private PlayerLook look;
+    private bool fullTurnComplete = false;
+
+    [SerializeField] private Animator weaponAnim;
+    [SerializeField] private AudioClip sfx;
+    [SerializeField] private AudioClip deploySound;
+    [SerializeField] private AudioClip confirmVoice;
+
+    public override bool CanInteract()
+    {
+        return !fullTurnComplete && TaskManager.Instance.currentPhase.tasks.Contains(task);
+    }
+
+    protected override void Interact(Transform player)
+    {
+        fullTurnComplete = true;
+        anim.SetBool("FullTurnComplete", fullTurnComplete);
+        DoomsdayStatusUI.Instance.weaponLeversPulled++;
+        SoundManager.Instance.PlaySFXOneShot(sfx, 0, 0.3f);
+
+        if(DoomsdayStatusUI.Instance.weaponLeversPulled == 2)
+        {
+            TaskManager.Instance.CompleteTask(task);
+            weaponAnim.SetTrigger("Deploy");
+            SoundManager.Instance.PlaySFXOneShot(deploySound, 0, 0.5f);
+            SoundManager.Instance.PlayShipPALine(confirmVoice);
+        }
+        
+    }
+}
