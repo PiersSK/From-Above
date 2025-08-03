488cf7595f376ad3396b1a7fffae5c81fac3cc83
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2025-08-03 21:31:47 +0100

    feat(code): updates UI functionality

--- Assets/Scripts/Interactables/Terminals/DiaryTerminal.cs
+++ Assets/Scripts/Interactables/Terminals/DiaryTerminal.cs
@@ -15,57 +15,22 @@ public class DiaryTerminal : Computer
     [Header("Progression Settings")]
     [SerializeField] TaskData task;
 
-    [Header("Audio Clips")]
-    [SerializeField] private AudioClip keysoundLight;
-    [SerializeField] private AudioClip keysoundHeavy;
-
     private int questionsAnswered = 0;
     private bool isLookingOut = false;
 
-    // String Constants
-    private const string HEADER = "===== Rapier 06 Observation Log =====\nAwaiting daily log for operation day: 0847...";
-    private List<string> QUESTIONS = new List<string>() {
-        "Question 01: When you look out, what do you see?",
-        "Question 02: When you look out, how do you feel?",
-        "Question 03: Do you see any signs of aggression from the enemy?"
-    };
-
-    private const string FOOTER = "LOG COMPLETED. Thank you for your continued vigilance. Please return tomorrow";
     private const string LOOKOUTWINDOW = "To look out the window";
     private const string RETURNTODIARY = "To enter your observations";
 
     private void Start()
     {
-        questionBlocks[0].GetComponent<DiaryQABlock>().questionText.text = QUESTIONS[0];
         questionBlocks[0].SetActive(true);
-        header.GetComponent<TextMeshProUGUI>().text = HEADER;
-        footer.GetComponent<TextMeshProUGUI>().text = FOOTER;
     }
 
     protected override void Update()
     {
-        if (playerAtComputer)
+        if (playerAtComputer && InputManager.Instance.playerActions.UIToggle.triggered)
         {
-            if (InputManager.Instance.playerActions.Submit.triggered && questionsAnswered < questionBlocks.Count)
-            {
-                string answer = questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.text;
-                RevealNextText(answer);
-            }
-
-            if (InputManager.Instance.playerActions.UIToggle.triggered)
-                ToggleLookout();
-        }
-
-        if (playerAtComputer && SoundManager.Instance.clipPlaying != initiationSound.name)
-        {
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                SoundManager.Instance.PlaySFXOneShot(keysoundHeavy);
-            }
-            else if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Mouse0) && !Input.GetKeyDown(KeyCode.Mouse1))
-            {
-                SoundManager.Instance.PlaySFXOneShot(keysoundLight, 0.2f);
-            }
+            ToggleLookout();
         }
 
         base.Update();
@@ -75,21 +40,20 @@ public class DiaryTerminal : Computer
     {
         base.Interact(player);
         UIManager.Instance.ShowToggleText(LOOKOUTWINDOW);
-        questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.Select();
+        if (questionsAnswered < questionBlocks.Count)
+            questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().answerButtons[0].Select();
     }
 
     protected override void SwitchToMouseKeyboard()
     {
         Cursor.lockState = CursorLockMode.None;
-        if(questionsAnswered < questionBlocks.Count)
-            questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.ActivateInputField();
     }
 
     protected override void SwitchToGamepad()
     {
         Cursor.lockState = CursorLockMode.Locked;
         if (questionsAnswered < questionBlocks.Count)
-            questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.ActivateInputField();
+            questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().answerButtons[0].Select();
     }
 
     private void ToggleLookout()
@@ -101,32 +65,25 @@ public class DiaryTerminal : Computer
             motor.ForcePlayerToPoint(lookoutPoint, true);
             Cursor.lockState = CursorLockMode.Locked;
             UIManager.Instance.ShowToggleText(RETURNTODIARY);
-            if (questionsAnswered < questionBlocks.Count)
-                questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.DeactivateInputField();
         } else
         {
             motor.ForcePlayerToPoint(lockPoint, true);
             Cursor.lockState = CursorLockMode.None;
             UIManager.Instance.ShowToggleText(LOOKOUTWINDOW);
             if (questionsAnswered < questionBlocks.Count)
-                questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.ActivateInputField();
+                questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().answerButtons[0].Select();
         }
     }
 
-    public void RevealNextText(string answer)
+    public void RevealNextQuestion()
     {
         UIManager.Instance.ClearSelectedUIObject();
-        questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.readOnly = true;
-        questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.interactable = false;
         questionsAnswered++;
 
         if (questionsAnswered < questionBlocks.Count)
         {
-            if (questionsAnswered < QUESTIONS.Count)
-                questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().questionText.text = QUESTIONS[questionsAnswered];
-
             questionBlocks[questionsAnswered].SetActive(true);
-            questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.ActivateInputField();
+            questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().answerButtons[0].Select();
         }
         else
         {
@@ -139,7 +96,5 @@ public class DiaryTerminal : Computer
     {
         base.ReleasePlayer();
         UIManager.Instance.HideToggleText();
-        if (questionsAnswered < questionBlocks.Count)
-            questionBlocks[questionsAnswered].GetComponent<DiaryQABlock>().inputField.DeactivateInputField();
     }
 }
--- Assets/Scripts/UI/Components/DiaryQABlock.cs
+++ Assets/Scripts/UI/Components/DiaryQABlock.cs
@@ -1,8 +1,20 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DiaryQABlock : TerminalInput
+public class DiaryQABlock : MonoBehaviour
 {
     public TextMeshProUGUI questionText;
+    public List<Button> answerButtons = new List<Button>();
+
+    public void LockInAnswer(Button btn)
+    {
+        btn.transform.localPosition = new Vector3(-32f, btn.transform.localPosition.y, 0f);
+        btn.interactable = false;
+        foreach(var button in answerButtons)
+        {
+            if (button != btn) button.gameObject.SetActive(false);
+        }
+    }
 }
