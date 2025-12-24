9fdff8ab6b1437a28d57f3f147e1ed65591d1f2d
OrcaOfTime <mwilliams.sean@gmail.com>
2025-12-24 18:08:30 +0000

    feat(code): Generic cleanable items
    To stop the broom becoming interactable outside of downtime phase. I've added it as an inheritor of the ClearableInteractables, like the shirt and notes. But a new bug is introduced as you can sweep multiple times, which adds to the 'cleaned items' count and can finish the task just by sweeping repeatedly. Going to have to tweak this to count or monitor something else so you can keep enjoying some sweeping action without completing the task early

--- Assets/Scripts/Interactables/Broom.cs
+++ Assets/Scripts/Interactables/Broom.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 
-public class Broom : Interactable
+public class Broom : ClearableInteractable
 {
-    [SerializeField] private RoomTidyCounter tidy;
-    [SerializeField] private AudioClip sfx;
     protected override void Interact(Transform player)
     {
         GetComponent<Animator>().SetTrigger("Sweep");
-        SoundManager.Instance.PlaySFXOneShot(sfx,0,0.2f);
-        tidy.swept = true;
+        base.Interact(player);
     }
 }
--- Assets/Scripts/Interactables/ClearableInteractable.cs
+++ Assets/Scripts/Interactables/ClearableInteractable.cs
@@ -5,14 +5,17 @@ public class ClearableInteractable : Interactable
 {
     [SerializeField] private RoomTidyCounter tidy;
     [SerializeField] private AudioClip sfx;
+    [SerializeField] private bool removable;
 
     protected override void Interact(Transform player)
     {
         if (tidy != null) {
-            tidy.objectsRemoved++;
+            tidy.objectsCleaned++;
         }
 
-        gameObject.SetActive(false);
+        if(removable) {
+          gameObject.SetActive(false);
+        }
         if (sfx != null) {
             SoundManager.Instance.PlaySFXOneShot(sfx, 0f, 0.4f);
         } 
